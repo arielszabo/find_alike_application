76e84547d1aa3f9c689a0e19069eee52937f3802
arielszabo <arielszabo@gmail.com>
2020-02-13 16:10:51 +0200

    fix ids and strings

--- app/src/main/java/com/szabgab/findalike/ReportMissingTitles.kt
+++ app/src/main/java/com/szabgab/findalike/ReportMissingTitles.kt
@@ -2,11 +2,21 @@ package com.szabgab.findalike
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class ReportMissingTitles : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report_missing_titles)
+
+
+        val missingTitleUrlText: TextView = findViewById(R.id.missing_title_url_text)
+        val sendMissingTitleButton: Button = findViewById(R.id.send_missing_title_button)
+
+//        sendMissingTitleButton.setOnClickListener {
+//             missingTitleUrlText.text // TODO send to the API
+//        }
     }
 }
