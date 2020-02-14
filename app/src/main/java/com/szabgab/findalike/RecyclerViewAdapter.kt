9867f46bfbcc9e68dc6849482383a834aac32e12
arielszabo <arielszabo@gmail.com>
2020-02-14 18:27:26 +0200

    use databinding at ViewHolder

--- app/src/main/java/com/szabgab/findalike/RecyclerViewAdapter.kt
+++ app/src/main/java/com/szabgab/findalike/RecyclerViewAdapter.kt
@@ -3,12 +3,8 @@ package com.szabgab.findalike
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.szabgab.findalike.databinding.TitleItemBinding
 
 class RecyclerViewAdapter(private val suggestedTitlesDataList: ArrayList<TitleData>): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
@@ -25,31 +21,22 @@ class RecyclerViewAdapter(private val suggestedTitlesDataList: ArrayList<TitleDa
 
     override fun getItemCount() = suggestedTitlesDataList.size
 
-    class ViewHolder private constructor (view: View): RecyclerView.ViewHolder(view) {
-        val expandableLayout: ConstraintLayout = itemView.findViewById(R.id.expandableLayout)
-
-        val movieTitleTextView: TextView = itemView.findViewById(R.id.movie_title)
-        val posterImageView: ImageView = itemView.findViewById(R.id.posterImageView)
-        val markAsSeenCheckBox: CheckBox = itemView.findViewById(R.id.markAsSeenCheckBox)
-        val plotTextView: TextView = itemView.findViewById(R.id.plot_text)
-        val ratingValueText: TextView = itemView.findViewById(R.id.rating_value_text)
-        val posterTitleAndCheckbox: LinearLayout = itemView.findViewById(R.id.poster_title_and_checkbox)
-
-
+    class ViewHolder private constructor (val binding: TitleItemBinding): RecyclerView.ViewHolder(binding.root) {
+        
         fun bindItems(titleData : TitleData, adapter: RecyclerViewAdapter){
-            movieTitleTextView.text = titleData.title
-            posterImageView.setImageBitmap(titleData.posterImage)
-            markAsSeenCheckBox.isChecked = titleData.isSeen
-            plotTextView.text = titleData.plot
-            ratingValueText.text = "9.9" // todo get from titleData
+            binding.movieTitle.text = titleData.title
+            binding.posterImageView.setImageBitmap(titleData.posterImage)
+            binding.markAsSeenCheckBox.isChecked = titleData.isSeen
+            binding.plotText.text = titleData.plot
+            binding.ratingValueText.text = "9.9" // todo get from titleData
 
-            posterTitleAndCheckbox.setOnClickListener {
+            binding.posterTitleAndCheckboxLayout.setOnClickListener {
                 titleData.isLayoutExpanded = !titleData.isLayoutExpanded
                 adapter.notifyItemChanged(adapterPosition)
             }
 
-            markAsSeenCheckBox.setOnClickListener {
-                val checkBoxStatus = markAsSeenCheckBox.isChecked
+            binding.markAsSeenCheckBox.setOnClickListener {
+                val checkBoxStatus = binding.markAsSeenCheckBox.isChecked
                 titleData.isSeen = checkBoxStatus // todo send to DB
                 // markAsSeenCheckBox.isChecked = checkBoxStatus
                 adapter.notifyItemChanged(adapterPosition)
@@ -57,16 +44,17 @@ class RecyclerViewAdapter(private val suggestedTitlesDataList: ArrayList<TitleDa
             }
 
             if (titleData.isLayoutExpanded) {
-                expandableLayout.visibility = View.VISIBLE
+                binding.expandableLayout.visibility = View.VISIBLE
             } else {
-                expandableLayout.visibility = View.GONE
+                binding.expandableLayout.visibility = View.GONE
             }
         }
 
         companion object {
             fun from(parent: ViewGroup): ViewHolder {
-                val view = LayoutInflater.from(parent.context).inflate(R.layout.title_item, parent, false)
-                return ViewHolder(view)
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = TitleItemBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
             }
         }
     }
