ba94373e19cc35957b7775ff9ea2bd28a833b2cb
arielszabo <arielszabo@gmail.com>
2020-02-14 13:17:57 +0200

    refactor

--- app/src/main/java/com/szabgab/findalike/RecyclerViewAdapter.kt
+++ app/src/main/java/com/szabgab/findalike/RecyclerViewAdapter.kt
@@ -14,29 +14,31 @@ import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerViewAdapter(private val suggestedTitlesDataList: ArrayList<TitleData>): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.title_item, parent, false)
-        return ViewHolder(v)
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder.from(parent)
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bindItems(suggestedTitlesDataList[position], this)
     }
 
+//    override fun getItemId(position: Int): Long {
+//        return super.getItemId(position)
+//        return suggestedTitlesDataList[position].imdbID // TODO maybe hash this to a long?
+//    }
+
     override fun getItemCount() = suggestedTitlesDataList.size
 
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    class ViewHolder private constructor (view: View): RecyclerView.ViewHolder(view) {
+        val expandableLayout: ConstraintLayout = itemView.findViewById(R.id.expandableLayout)
+
+        val movieTitleTextView: TextView = itemView.findViewById(R.id.movie_title)
+        val posterImageView: ImageView = itemView.findViewById(R.id.posterImageView)
+        val markAsSeenCheckBox: CheckBox = itemView.findViewById(R.id.markAsSeenCheckBox)
+        val plotTextView: TextView = itemView.findViewById(R.id.plot_text)
+        val ratingValueText: TextView = itemView.findViewById(R.id.rating_value_text)
+        val posterTitleAndCheckbox: LinearLayout = itemView.findViewById(R.id.poster_title_and_checkbox)
+
 
         fun bindItems(titleData : TitleData, adapter: RecyclerViewAdapter){
-            val expandableLayout: ConstraintLayout = itemView.findViewById(R.id.expandableLayout)
-
-            val movieTitleTextView: TextView = itemView.findViewById(R.id.movie_title)
-            val posterImageView: ImageView = itemView.findViewById(R.id.posterImageView)
-            val markAsSeenCheckBox: CheckBox = itemView.findViewById(R.id.markAsSeenCheckBox)
-            val plotTextView: TextView = itemView.findViewById(R.id.plot_text)
-            val ratingValueText: TextView = itemView.findViewById(R.id.rating_value_text)
-            val posterTitleAndCheckbox: LinearLayout = itemView.findViewById(R.id.poster_title_and_checkbox)
-
             movieTitleTextView.text = titleData.title
             posterImageView.setImageBitmap(titleData.posterImage)
             markAsSeenCheckBox.isChecked = titleData.isSeen
@@ -63,5 +65,11 @@ class RecyclerViewAdapter(private val suggestedTitlesDataList: ArrayList<TitleDa
             }
         }
 
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.title_item, parent, false)
+                return ViewHolder(view)
+            }
+        }
     }
 }
