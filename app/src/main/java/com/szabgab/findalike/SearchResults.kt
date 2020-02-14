608b9ee1bbdc44fd8e5d320032022ff0741e2e0f
arielszabo <arielszabo@gmail.com>
2020-02-14 13:18:12 +0200

    add data bindings to SearchResults

--- app/src/main/java/com/szabgab/findalike/SearchResults.kt
+++ app/src/main/java/com/szabgab/findalike/SearchResults.kt
@@ -3,19 +3,23 @@ package com.szabgab.findalike
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.szabgab.findalike.databinding.ActivitySearchResultsBinding
 
 class SearchResults : AppCompatActivity() {
 
     // TODO onSaveInstanceState to fix device rotation data lose
 
+    private lateinit var binding: ActivitySearchResultsBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search_results)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val recyclerView: RecyclerView = findViewById(R.id.recyclerview)
-        recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL,
+        binding.recyclerview.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL,
             false)
 
         val suggestedTitles = ArrayList<TitleData>()
@@ -27,15 +31,17 @@ class SearchResults : AppCompatActivity() {
         for (i in 0..150) {
             val year = 2_000 + i
             val suggestedTitleData = TitleData("$chosenTitle was chosen",
-                BitmapFactory.decodeResource(resources, R.drawable.example_icon),
+                "director name",
                 year,
                 "Some long long plot to describe the movie",
+                BitmapFactory.decodeResource(resources, R.drawable.example_icon),
+                "imdbid",
                 "http://imdb.com/ttsomething")
             suggestedTitles.add(suggestedTitleData)
 
         }
 
         val adapter = RecyclerViewAdapter(suggestedTitles)
-        recyclerView.adapter = adapter
+        binding.recyclerview.adapter = adapter
     }
 }
